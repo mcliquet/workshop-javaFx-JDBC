4f3dc4bb0522c4e4ef459d0b109a684d477be8c6
Marcus <cliquet@gmail.com>
2019-07-17 14:57:04 -0300

    DepartmentList controller

---
+++ src/model/entities/Department.java
@@ -0,0 +1,69 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+	//para os objetos poderem ser transformados em sequencia de bytes, para ser gravado em arquivo ou trafegado em rede
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	
+	public Department() {
+	}
+	
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//para os objetos poderem ser comparados pelo conteúno e não pelo apontamento na memória
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+
+
+
+}
