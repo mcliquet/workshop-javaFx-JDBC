fa5307067f37b841733d5b70d6d0fbd202ca5197
Marcus <cliquet@gmail.com>
2019-07-17 14:30:36 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,22 +9,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch (IOException e) {
+			
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
